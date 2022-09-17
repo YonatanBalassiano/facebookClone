4f07bec624cb0f74eb3939978ed407ec4ca826a6
Yonatan <101994161+YonatanBalassiano@users.noreply.github.com>
2022-09-17 15:55:30 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,49 +1,83 @@
-const express = require('express');
-const app = express();
-const port = 3003;
-const middleware = require('./middleware')
-const path = require('path')
-const bodyParser = require("body-parser")
-const mongoose = require("./database");
+const express = require("express");
+const bodyParser = require("body-parser");
 const session = require("express-session");
 
-const server = app.listen(port, () => console.log("Server listening on port " + port));
 
+const app = express();
+
+
+//database connection
+const mongoose = require("mongoose");
+const { homedir } = require("os");
+mongoose.connect("mongodb://localhost:27017/FacebookCloneDB", {useNewUrlParser: true});
+
+
+//initialize 
 app.set("view engine", "pug");
-app.set("views", "views");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-
+app.set("views","views");
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + "/public"));
 app.use(session({
     secret: "bbq chips",
     resave: true,
     saveUninitialized: false
 }))
 
-// Routes
-const loginRoute = require('./routes/loginRoutes');
-const registerRoute = require('./routes/registerRoutes');
-const postRoute = require('./routes/postRoutes');
-const profileRoute = require('./routes/profileRoutes');
 
-// Api routes
-const postsApiRoute = require('./routes/api/posts');
+
+//port listening
+app.listen ("3005", function(){
+    console.log("listen on port 3005");
+})
+
+//Routes
+const loginRoute = require("./routes/loginRoute");
+const registerRoute = require("./routes/registerRoute");
+const logoutRoute = require("./routes/logoutRoute");
+const resetRoute = require("./routes/resetRoute");
+const profilePageRoute = require("./routes/profilePageRoute");
+const searchRoute = require("./routes/searchRoute");
+const messagesRoute = require("./routes/messagesRoute");
+
+
+//API routes
+const postsApiRoutes = require("./routes/api/posts");
+const usersApiRoutes = require("./routes/api/users");
+const ChatsApiRoutes = require("./routes/api/chats");
+const MessagesApiRoutes = require("./routes/api/messages");
+
 
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
-app.use("/posts", middleware.requireLogin, postRoute);
-app.use("/profile", middleware.requireLogin, profileRoute);
+app.use("/logout", logoutRoute);
+app.use("/reset", resetRoute);
+app.use("/profile", profilePageRoute);
+app.use("/search", searchRoute);
+app.use("/messages", messagesRoute);
+app.use("/api/posts", postsApiRoutes);
+app.use("/api/users", usersApiRoutes);
+app.use("/api/Chats", ChatsApiRoutes);
+app.use("/api/messages", MessagesApiRoutes);
 
-app.use("/api/posts", postsApiRoute);
 
-app.get("/", middleware.requireLogin, (req, res, next) => {
 
-    var payload = {
+app.get("/", function(req, res){
+    if(req.session && req.session.user){
+        let payload = {
         pageTitle: "Home",
-        userLoggedIn: req.session.user,
-        userLoggedInJs: JSON.stringify(req.session.user),
+        userLoggedIn:req.session.user,
+        userLoggedInJS: JSON.stringify(req.session.user)
     }
 
-    res.status(200).render("home", payload);
+    res.render("home",payload);
+    }
+    else{
+        res.redirect("/Login")
+    }
 })
+
+
+
+
+
+
